942c4a2deba384ab6e7430bbfe1f09c6bfffaf27
Vinicius Maciel <vinicius_ceth@yahoo.com.br>
2012-05-23 07:18:06 -0700

    paciente done

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120523130548) do
+ActiveRecord::Schema.define(:version => 20120523141711) do
 
   create_table "atendentes", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -56,5 +56,26 @@ ActiveRecord::Schema.define(:version => 20120523130548) do
 
   add_index "medicos", ["email"], :name => "index_medicos_on_email", :unique => true
   add_index "medicos", ["reset_password_token"], :name => "index_medicos_on_reset_password_token", :unique => true
+
+  create_table "pacientes", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "nome"
+    t.text     "endereco"
+    t.string   "CPF"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "pacientes", ["email"], :name => "index_pacientes_on_email", :unique => true
+  add_index "pacientes", ["reset_password_token"], :name => "index_pacientes_on_reset_password_token", :unique => true
 
 end
