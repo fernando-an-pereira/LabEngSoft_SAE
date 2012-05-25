7a250430a444105349e340906c24c7225f6c67c6
Midori Hirami <stmidori@gmail.com>
2012-05-25 10:57:27 -0700

    teste2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120523144517) do
+ActiveRecord::Schema.define(:version => 20120523150303) do
 
   create_table "atendentes", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -83,6 +83,15 @@ ActiveRecord::Schema.define(:version => 20120523144517) do
     t.date     "data"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "registro_de_emergencia", :force => true do |t|
+    t.datetime "data"
+    t.text     "localidadeEmergencia"
+    t.text     "gravidade"
+    t.text     "causas"
+    t.datetime "created_at",           :null => false
+    t.datetime "updated_at",           :null => false
   end
 
 end
