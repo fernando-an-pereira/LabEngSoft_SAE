4a49cddbb04a6deacf97b93601bd88c860b67bb1
Giovanni Gatti <giovanni.gatti.pinheiro@gmail.com>
2012-06-06 06:14:38 -0700

    Corrigindo uma  besteira que fiz.

--- app/models/prontuario.rb
+++ app/models/prontuario.rb
@@ -2,7 +2,7 @@ class Prontuario < ActiveRecord::Base
   attr_accessible :data, :observacao
   
   belongs_to :paciente
-  has_many :registro_de_emergencium
-  has_many :prescricao
-  has_many :exame
+  has_many :registro_de_emergencia
+  has_many :prescricaos
+  has_many :exames
 end
